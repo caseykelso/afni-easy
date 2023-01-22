ca54af105145a84b6f5d4efb3f235d19f6f49279
Casey Kelso <casey@caseykelso.com>
2023-01-21 21:22:13 -0800

    * set working directory to afni

--- run.sh
+++ run.sh
@@ -8,4 +8,4 @@ TAG=0.01
 FORCE_BUILD=0
 PRIVILEGED=
 ENVIRONMENT=
-	xhost +local:docker && docker run $PRIVILEGED --net=host  -e DISPLAY=$DISPLAY -v /root/.Xauthority:/root/.Xauthority:rw -v /tmp/.X11-unix:/tmp/.X11-unix  -v $SOURCE:$CONTAINER_HOME/dev:delegated  -v  ~/.ssh:/home/build/.ssh:ro -v ~/.gitconfig:/home/build/.gitconfig:ro  -it  $REPOSITORY
+	xhost +local:docker && docker run $PRIVILEGED --net=host  -w /root/abin -e DISPLAY=$DISPLAY -v /root/.Xauthority:/root/.Xauthority:rw -v /tmp/.X11-unix:/tmp/.X11-unix  -v $SOURCE:$CONTAINER_HOME/dev:delegated  -v  ~/.ssh:/home/build/.ssh:ro -v ~/.gitconfig:/home/build/.gitconfig:ro  -it  $REPOSITORY
