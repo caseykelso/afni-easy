e76feb6d7e188718f624fd68379d0360f5046dd1
Casey Kelso <casey@caseykelso.com>
2023-01-25 20:25:13 -0800

    * set Qt4 rendering to native, if this doesn't work try export QT_QPA_PLATFORM=offscreen

--- run.sh
+++ run.sh
@@ -8,4 +8,4 @@ TAG=0.01
 FORCE_BUILD=0
 PRIVILEGED=
 ENVIRONMENT=
-	xhost +local:docker && docker run $PRIVILEGED --net=host  -w /root/abin -e DISPLAY=$DISPLAY -v /root/.Xauthority:/root/.Xauthority:rw -v /tmp/.X11-unix:/tmp/.X11-unix  -v $SOURCE:$CONTAINER_HOME/dev:delegated  -v  ~/.ssh:/root/.ssh:ro -v ~/.gitconfig:/root/.gitconfig:ro  -it  $REPOSITORY
+	xhost +local:docker && docker run $PRIVILEGED --net=host  -w /root/abin -e QT_GRAPHICSSYSTEM="native" -e DISPLAY=$DISPLAY -v /root/.Xauthority:/root/.Xauthority:rw -v /tmp/.X11-unix:/tmp/.X11-unix  -v $SOURCE:$CONTAINER_HOME/dev:delegated  -v  ~/.ssh:/root/.ssh:ro -v ~/.gitconfig:/root/.gitconfig:ro  -it  $REPOSITORY
